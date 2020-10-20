ff583271f205ab55866904b738529f58082db81f
daniel.amaral <amaral94daniel@gmail.com>
2020-10-20 18:37:08 +0100

    small fixes

--- index.js
+++ index.js
@@ -12,49 +12,60 @@ var analyzeTask = (input) => {
     var inputComponents = input.replace(/\n$/, "").split("\n");
     var numberOfUsers = parseInt(inputComponents[0]);
     var i = 1;
+    var numberOfTasks = 0;
 
     for (i = 1; i < numberOfUsers + 1; i++) {
       var splitUserInfo = inputComponents[i].split(" ");
 
-      if (!Object.prototype.hasOwnProperty.call(tasks, splitUserInfo[0])) {
-        tasks[splitUserInfo[0]] = {
-          totalTime: 0,
-          occurrences: 0,
-          countryCode: splitUserInfo[1],
-        };
-      }
-      if (!Object.prototype.hasOwnProperty.call(countries, splitUserInfo[1])) {
-        countries[splitUserInfo[1]] = {
-          totalTime: 0,
-          occurrences: 0,
-        };
+      if (splitUserInfo.length == 2) {
+        if (!Object.prototype.hasOwnProperty.call(tasks, splitUserInfo[0])) {
+          tasks[splitUserInfo[0]] = {
+            totalTime: 0,
+            occurrences: 0,
+            countryCode: splitUserInfo[1],
+          };
+        }
+        if (!Object.prototype.hasOwnProperty.call(countries, splitUserInfo[1])) {
+          countries[splitUserInfo[1]] = {
+            totalTime: 0,
+            occurrences: 0,
+          };
+        }
+      } else {
+        return "Invalid input"
       }
     }
 
+    numberOfTasks = parseInt(inputComponents[i]) + i;
     i += 1;
-    for (i; i < inputComponents.length; i++) {
+    for (i; i < numberOfTasks + 1; i++) {
       var splitTaskInfo = inputComponents[i].split(" ");
 
-      if (!Object.prototype.hasOwnProperty.call(tasks, splitTaskInfo[1])) {
-        tasks[splitTaskInfo[1]] = {
-          totalTime: parseInt(splitTaskInfo[2]),
-          occurrences: 1,
-        };
-      } else {
-        tasks[splitTaskInfo[1]].totalTime += parseInt(splitTaskInfo[2]);
-        tasks[splitTaskInfo[1]].occurrences += 1;
+      if (splitTaskInfo.length == 3) {
+        if (!Object.prototype.hasOwnProperty.call(tasks, splitTaskInfo[1])) {
+          tasks[splitTaskInfo[1]] = {
+            totalTime: parseInt(splitTaskInfo[2]),
+            occurrences: 1,
+          };
+        } else {
+          tasks[splitTaskInfo[1]].totalTime += parseInt(splitTaskInfo[2]);
+          tasks[splitTaskInfo[1]].occurrences += 1;
+        }
+
+        var countryCode = tasks[splitTaskInfo[1]].countryCode;
+
+        if (!Object.prototype.hasOwnProperty.call(countries, countryCode)) {
+          countries[countryCode] = {
+            totalTime: parseInt(splitTaskInfo[2]),
+            occurrences: 1,
+          };
+        } else {
+          countries[countryCode].totalTime += parseInt(splitTaskInfo[2]);
+          countries[countryCode].occurrences += 1;
+        }
       }
-
-      var countryCode = tasks[splitTaskInfo[1]].countryCode;
-
-      if (!Object.prototype.hasOwnProperty.call(countries, countryCode)) {
-        countries[countryCode] = {
-          totalTime: parseInt(splitTaskInfo[2]),
-          occurrences: 1,
-        };
-      } else {
-        countries[countryCode].totalTime += parseInt(splitTaskInfo[2]);
-        countries[countryCode].occurrences += 1;
+      else {
+        return "Invalid input"
       }
     }
 
